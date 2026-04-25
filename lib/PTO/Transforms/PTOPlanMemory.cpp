2a7692b669f8ae2f13eb226bc46efd1d1624d48e
FangRui <fangrui_95@163.com>
2026-04-25 17:08:48 +0800

    feat:  add cube dma laod/store ops

--- lib/PTO/Transforms/PTOPlanMemory.cpp
+++ lib/PTO/Transforms/PTOPlanMemory.cpp
@@ -460,7 +460,8 @@ void MemLivenessAnalysis::RecursionIR(Region *region, Liveness live) {
     } else if (isa<pto::TAllocOp, pto::TPushOp, pto::TFreeOp,
                    pto::InitializeL2LPipeOp, pto::InitializeL2G2LPipeOp,
                    pto::BuildAsyncSessionOp,
-                   pto::TPutAsyncOp, pto::TGetAsyncOp>(op)) {
+                   pto::TPutAsyncOp, pto::TGetAsyncOp,
+                   pto::CubeLoadFracOp>(op)) {
       UpdateOpGenInfo(curOpInfo, llvm::to_vector(op->getOperands()));
       OpKillHandle(curOpInfo, live, op->getBlock());
     } else if (auto gpuLaunchOp = dyn_cast<gpu::LaunchFuncOp>(op)) {
