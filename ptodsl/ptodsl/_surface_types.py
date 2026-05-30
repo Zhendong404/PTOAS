20b1a5453aaf789d9c1f6c2e2e24a524a3676b3e
Zhendong404 <zhendong404@gmail.com>
2026-05-30 15:03:14 +0800

    feat(ptodsl): Support generating mixed-backend kernel module

--- ptodsl/ptodsl/_surface_types.py
+++ ptodsl/ptodsl/_surface_types.py
@@ -10,7 +10,6 @@
 from enum import Enum
 
 from ._bootstrap import make_context  # noqa: F401
-from ._host_tensors import TensorSpec, tensor_spec
 
 from mlir.dialects import pto as _pto
 
@@ -285,7 +284,6 @@ class Tile:
 
 __all__ = [
     "constexpr",
-    "TensorSpec",
     "MemorySpace",
     "BarrierType",
     "Pipe",
@@ -319,5 +317,4 @@ __all__ = [
     "TensorView",
     "PartitionTensorView",
     "Tile",
-    "tensor_spec",
 ]
